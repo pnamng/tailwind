501d9fa3a37af4baf99c9093e95d3b6dec97aece
sotatek <pnam2311@gmail.com>
2021-07-08 11:32:13 +0700

    Initialize

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>aaa</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/card/Card.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+const Card: FC = () => {
+  console.log('aaa');
+  return (
+    <>
+      <div className="w-32 h-32">aaa</div>
+    </>
+  );
+};
+
+export default Card;
