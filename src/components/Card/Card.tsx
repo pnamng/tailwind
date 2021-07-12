94de71d3277fc0ee0137a20cc85fc862d1409603
Nam Nguyen <pnam2311@gmail.com>
2021-07-12 16:49:17 +0700

    tsconfig.json

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,9 @@
-import React, { FC } from 'react';
-import PopUp from './PopUp';
+import React from 'react';
 
 const Card = () => {
-  console.log('aaa');
   return (
     <>
       <div className="w-32 h-32">aaa</div>
-      <PopUp
     </>
   );
 };
